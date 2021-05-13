df2e5578f2f933cc329072698f8df2de65b71a19
yuya-taniguti <yuya2624@gmail.com>
2021-05-13 18:49:29 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_k, presence: true
+  validates :last_name_k, presence: true
+  validates :birthday_year, presence: true
 end

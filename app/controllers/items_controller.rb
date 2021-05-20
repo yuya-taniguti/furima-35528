b070d25ef59ed155eaf02116687a87c899fcb2b3
yuya-taniguti <yuya2624@gmail.com>
2021-05-20 14:11:19 +0900

    修正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, , :destroy]
 
   def index
      @items = Item.all.order ("created_at DESC")

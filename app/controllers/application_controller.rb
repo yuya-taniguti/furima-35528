87db5e8b71d948a068b0465a3da6c4ee028e685e
yuya-taniguti <yuya2624@gmail.com>
2021-05-21 10:42:48 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
+  
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :last_name, :first_name, :last_name_k, :first_name_k, :birthday_year])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'yuyataniguchi' && password == '5468'
+    end
   end
 end

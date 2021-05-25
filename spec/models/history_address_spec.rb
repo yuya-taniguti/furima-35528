98530b440562e623810522ffc29f1855002f3102
yuya-taniguti <yuya2624@gmail.com>
2021-05-25 13:00:48 +0900

    修正４回目

--- spec/models/history_address_spec.rb
+++ spec/models/history_address_spec.rb
@@ -130,12 +130,6 @@ RSpec.describe HistoryAddress, type: :model do
         @history_address.valid?
         expect(@history_address.errors.full_messages).to include "Item can't be blank"
       end
-
-      it "tokenが空では登録できないこと" do
-        @history_address.token = nil
-        @history_address.valid?
-        expect(@history_address.errors.full_messages).to include "Token can't be blank"
-      end
     end
   end
 end

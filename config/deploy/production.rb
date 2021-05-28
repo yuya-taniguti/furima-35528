66f39ad7349ddee594e23ecef8c124e0a667e976
yuya-taniguti <yuya2624@gmail.com>
2021-05-28 17:29:09 +0900

    修正２回目

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 35.73.123.54, user: 'ec2-user', roles: %w{app db web}
+server '35.73.123.54', user: 'ec2-user', roles: %w{app db web}

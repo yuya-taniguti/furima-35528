05c8a12976f85c848d4f709790e8d32868442cbc
yuya-taniguti <yuya2624@gmail.com>
2021-05-28 17:27:34 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 3.16.0
+lock '3.16.0'
 
 # Capistranoのログの表示に利用する
-set :application, furima-35528
+set :application, 'furima-35528'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yuya-taniguti/furima-35528.git'
@@ -11,7 +11,7 @@ set :repo_url,  'git@github.com:yuya-taniguti/furima-35528.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.6.5 #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
